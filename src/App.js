1c691535352840d390d2728581fb3b0bbdeac22c
Dave Darsa <davedabyeet@gmail.com>
2021-03-08 18:30:38 -0400

    Decluttering adding packages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header"></header>
+      <div className="lol">testing</div>
+    </div>
+  );
+}
+
+export default App;
